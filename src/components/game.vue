138ba306e9451a2171a0d03fe91e84487cbad5ed
Daniel <daniel@rydstroms.com>
2018-08-23 22:22:16 +0200

    Adding files generated by vue init.

---
+++ src/components/game.vue
@@ -0,0 +1,37 @@
+<!-- src/components/Game.vue -->
+
+<template>
+<div class="game-block">
+<span>{{title}}</span>
+<span>{{price}}</span>
+<span>{{currency}}</span>
+<span>{{description}}</span>
+<img src="{{thumbnail}}" />
+<button class="btn btn-primary" v-on:click="getGame()">Fetch game</button>
+<div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  data () {
+    return {
+      title: '',
+      price: '',
+      currency: '',
+      description: '',
+      thumbnail: ''
+    }
+  },
+  methods: {
+    getGame () {
+      this.$http
+        .get('https://store.steampowered.com/api/appdetails?appids=57690&l=sv', (data) => {
+          console.log(data)
+          // this.title = data;
+        })
+        .error((err) => console.log(err))
+    }
+  }
+}
+</script>
