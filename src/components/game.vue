52ba0d12edcfa4846ceaf895326eb4fd12e138b0
Daniel <daniel@rydstroms.com>
2018-09-02 17:27:34 +0200

    Having issues with CORS request. Might not continue this work. We'll see...

--- src/components/game.vue
+++ src/components/game.vue
@@ -6,9 +6,9 @@
 <span>{{price}}</span>
 <span>{{currency}}</span>
 <span>{{description}}</span>
-<img src="{{thumbnail}}" />
+<img :src="thumbnail" />
 <button class="btn btn-primary" v-on:click="getGame()">Fetch game</button>
-<div>
+</div>
 </template>
 
 <script>
@@ -25,13 +25,41 @@ export default {
   },
   methods: {
     getGame () {
-      this.$http
-        .get('https://store.steampowered.com/api/appdetails?appids=57690&l=sv', (data) => {
-          console.log(data)
-          // this.title = data;
-        })
-        .error((err) => console.log(err))
+      console.log('getGame')
+
+      var invocation = new XMLHttpRequest()
+      var url = 'https://store.steampowered.com/api/appdetails?appids=57690&l=sv'
+
+      function handler (response) {
+        console.log(response)
+      }
+
+      function callOtherDomain () {
+        if (invocation) {
+          invocation.open('GET', url, true)
+          invocation.onreadystatechange = handler
+          invocation.send()
+        }
+      }
+      callOtherDomain()
+
+      // this.$http
+      //   .get('https://store.steampowered.com/api/appdetails?appids=57690&l=sv', (data) => {
+      //     console.log(data)
+      //     // this.title = data;
+      //   })
+      //   .error((err) => console.log(err))
     }
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.game-block {
+  background: #ccc;
+  display: inline-block;
+  min-width: 100px;
+  min-height: 100px;
+}
+</style>
